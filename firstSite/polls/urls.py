c52339f6b4cc43c9c9bb00bf1e894685efaef2f2
ombansal <om.bansal05@gmail.com>
2019-01-04 17:34:33 +0530

    add templates

--- firstSite/polls/urls.py
+++ firstSite/polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
             path('', views.index, name='index'),
+            # /polls/5/
+            path('<int:question_id>/', views.detail, name='detail'),
 ]
--- firstSite/polls/views.py
+++ firstSite/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+#from django.template import loader
+from .models import Question
 
 def index(req):
-    return HttpResponse("Hello, This is your first page.")
-
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    #output = ','.join([q.question_text for q in latest_question_list])
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #return HttpResponse(template.render(context, req))
+    return render(req, 'polls/index.html', context)
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
